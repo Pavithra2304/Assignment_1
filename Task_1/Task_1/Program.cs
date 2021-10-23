046c339633ac648715e73a0df4957a6cec7c570d
Pavithra2304 <92969170+Pavithra2304@users.noreply.github.com>
2021-10-23 07:54:43 +0530

    Add files via upload

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			String name;
+			String address;
+			String mobileno;
+			String mailid;
+			String qualification;
+			String date;
+			Console.WriteLine("Enter your Name :");
+			name = Console.ReadLine();
+			Console.WriteLine("Enter your Address :");
+			address = Console.ReadLine();
+			Console.WriteLine("Enter your MobileNo :");
+			mobileno = Console.ReadLine();
+			Console.WriteLine("Enter your MailId :");
+			mailid = Console.ReadLine();
+			Console.WriteLine("Enter your Qualification :");
+			qualification = Console.ReadLine();
+			Console.WriteLine("Enter today Date :");
+			date = Console.ReadLine();
+			Console.WriteLine("---------------------------------");
+			Console.WriteLine(name);
+			Console.WriteLine(address);
+			Console.WriteLine(mobileno);
+			Console.WriteLine(mailid);
+			Console.WriteLine(qualification);
+			Console.WriteLine(date);
+			Console.Read();
+			
+		}
+	}
+}
